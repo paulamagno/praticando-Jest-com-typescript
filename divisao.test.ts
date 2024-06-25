04fc0abf1c455b4eaf851c1dd5374b3d6c90e1d2
Paula Vitoria <paulavitoriabrasil@gmail.com>
2024-06-25 20:28:43 -0300

    implementação da função divisão com teste

---
+++ divisao.test.ts
@@ -0,0 +1,13 @@
+import divisao from './divisao'
+
+describe ('Teste de divisão', () => {
+    test ('deve verificar o resultado da divisão', () => {
+        expect(divisao(6.6, 2)).toBeCloseTo(3.3)
+        expect(divisao(8, 8)).toBeCloseTo(1)
+        expect(divisao(60, 2)).toBeCloseTo(30)
+    })
+
+    test ('deve verificar o resultado incorreto da divisão', () => {
+        expect(divisao(6, 2)).not.toBeCloseTo(20)
+    })
+})
