df08b8198758a2657fe7d1dcda5e87108b835a12
Paula Vitoria <paulavitoriabrasil@gmail.com>
2024-06-25 18:16:43 -0300

    implementação da função subtração com testes

---
+++ subtracao.test.ts
@@ -0,0 +1,15 @@
+import subtracao from './subtracao'
+
+describe('Testes de subtração', () =>{
+    test('deve verificar o resultado de uma subtração', () => {
+        expect(subtracao(6, 2)).toBeCloseTo(4)
+        expect(subtracao(6, 8)).toBeCloseTo(-2)
+        expect(subtracao(60, 2)).toBeCloseTo(58)
+    })
+
+    test('deve verificar o resultado incorreto de uma subtração', () => {
+        expect(subtracao(6, 2)).not.toBe(5)
+        expect(subtracao(6, 8)).not.toBe(-3)
+        expect(subtracao(60, 2)).not.toBe(60)
+    })
+})
